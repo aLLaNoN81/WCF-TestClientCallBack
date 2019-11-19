157b9987670e4bb88d561af42cf1265cd4070d90
aLLaNoN81 <45932395+aLLaNoN81@users.noreply.github.com>
2019-11-19 15:07:29 +0100

    Initial commit

---
+++ TestClientCallBack/CallBackClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using TestClientCallBack.WCFServiceCallback;
+
+namespace TestClientCallBack
+{
+    public class RequestCallBack : IServiceCallback
+    {
+        public void SendResult()
+        {
+            MessageBox.Show("Received!");
+        }
+    }
+}
---
+++ TestClientCallBack/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceModel;
+using System.Windows;
+using TestClientCallBack.WCFServiceCallback;
+
+namespace TestClientCallBack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RequestCallBack _request;
+        private InstanceContext _instance;
+        private ServiceClient _proxy;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _request = new RequestCallBack();
+            _instance = new InstanceContext(_request);
+            _proxy = new ServiceClient(_instance);
+            _proxy.GetData();
+        }
+    }
+}
